4f3f80c1429a66892c5fbf75c5178be16aafa365
Vanessa W <73565294+Macrina1984@users.noreply.github.com>
2022-07-20 18:39:43 -0400

    create landing page for resume builder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import LandingPage from "./Components/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="Container">
+        <LandingPage />
+        <a href="./App.js" target="_blank">
+          <button className="btn btn-next">Next</button>
         </a>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ src/Components/LandingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <h1>Create a Winning Resume</h1>
+      <h3>
+        Build an easily custimizable and simple resume in a few clicks! Our
+        resume builder is easy to use with a range of functions tailored to meet
+        the needs of U.S. military personnel transitioning out of the military
+        and veterans. Custom-tailor resumes for any job within minutes!
+      </h3>
+    </div>
+  );
+}
