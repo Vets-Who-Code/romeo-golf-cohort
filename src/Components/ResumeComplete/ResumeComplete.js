f4acefde7589588d9b3cf74954bd9f6bcfb794b0
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2022-07-27 14:49:06 -0500

    Feat():Added Page that will display once the user has generated the resume

---
+++ src/Components/ResumeComplete/ResumeComplete.js
@@ -0,0 +1,47 @@
+import React from "react";
+import CheckCircle from "../../images/check-circle.png";
+import "./ResumeComplete.css";
+import "../../App.css";
+
+function ResumeComplete() {
+  return (
+    <>
+      <div className="container">
+        
+        
+        <div className="rc-response">
+            <img src={CheckCircle} alt="" />
+              <h1>Here's Your Resume!</h1>
+              <h3>
+                Now that you have an awesome resume, time to go get that dream job!
+              </h3>
+              <div className="rc-response__action">
+                <a href=" ">
+                    <button className="btn btn-cr">
+                    Create New Resume
+                    </button>
+                </a>
+                <a href="https://vetswhocode.io" id="rtrn-vwc">
+                    Back to VWC
+                </a>
+              </div>
+          
+        </div>
+     
+      
+        <div className="dwnld-error">
+          <p>
+            If your resume did not automatically download, <a href=" " id="dwnld-error__link">click here</a> to
+            download it.
+          </p>
+        </div>
+      </div>
+
+      {/* 
+          
+         */}
+    </>
+  );
+}
+
+export default ResumeComplete;
